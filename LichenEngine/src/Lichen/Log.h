c9d5651de8e02923f5defa0eb622e7e8092dc390
Lisha from Linux-Mint-MATE-OLDHP <lishastudies.cs@gmail.com>
2026-03-15 11:55:03 +0530

    Updated cmake build system, added Distro config

--- LichenEngine/src/Lichen/Log.h
+++ LichenEngine/src/Lichen/Log.h
@@ -16,16 +16,29 @@ namespace Lichen {
    };
 }
 
-#define LCH_CORE_TRACE(...)   Lichen::Log::GetCoreLogger()->trace(__VA_ARGS__)
-#define LCH_CORE_INFO(...)    Lichen::Log::GetCoreLogger()->info(__VA_ARGS__)
-#define LCH_CORE_DEBUG(...)   Lichen::Log::GetCoreLogger()->debug(__VA_ARGS__)
-#define LCH_CORE_WARN(...)    Lichen::Log::GetCoreLogger()->warn(__VA_ARGS__)
-#define LCH_CORE_ERROR(...)   Lichen::Log::GetCoreLogger()->error(__VA_ARGS__)
-#define LCH_CORE_FATAL(...)   Lichen::Log::GetCoreLogger()->critical(__VA_ARGS__)
+#ifndef LCH_CFG_DISTRO
 
-#define LCH_TRACE(...)   Lichen::Log::GetClientLogger()->trace(__VA_ARGS__)
-#define LCH_INFO(...)    Lichen::Log::GetClientLogger()->info(__VA_ARGS__)
-#define LCH_DEBUG(...)   Lichen::Log::GetClientLogger()->debug(__VA_ARGS__)
-#define LCH_WARN(...)    Lichen::Log::GetClientLogger()->warn(__VA_ARGS__)
-#define LCH_ERROR(...)   Lichen::Log::GetClientLogger()->error(__VA_ARGS__)
-#define LCH_FATAL(...)   Lichen::Log::GetClientLogger()->critical(__VA_ARGS__)
+   #define LCH_CORE_TRACE(...)   Lichen::Log::GetCoreLogger()->trace(__VA_ARGS__)
+   #define LCH_CORE_INFO(...)    Lichen::Log::GetCoreLogger()->info(__VA_ARGS__)
+   #define LCH_CORE_DEBUG(...)   Lichen::Log::GetCoreLogger()->debug(__VA_ARGS__)
+   #define LCH_CORE_WARN(...)    Lichen::Log::GetCoreLogger()->warn(__VA_ARGS__)
+   #define LCH_CORE_ERROR(...)   Lichen::Log::GetCoreLogger()->error(__VA_ARGS__)
+   #define LCH_CORE_FATAL(...)   Lichen::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+#else
+
+   #define LCH_CORE_TRACE
+   #define LCH_CORE_INFO
+   #define LCH_CORE_DEBUG  
+   #define LCH_CORE_WARN
+   #define LCH_CORE_ERROR
+   #define LCH_CORE_FATAL
+
+#endif
+
+#define LCH_TRACE(...)           Lichen::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define LCH_INFO(...)            Lichen::Log::GetClientLogger()->info(__VA_ARGS__)
+#define LCH_DEBUG(...)           Lichen::Log::GetClientLogger()->debug(__VA_ARGS__)
+#define LCH_WARN(...)            Lichen::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define LCH_ERROR(...)           Lichen::Log::GetClientLogger()->error(__VA_ARGS__)
+#define LCH_FATAL(...)           Lichen::Log::GetClientLogger()->critical(__VA_ARGS__)
