91d88826763aa81e5698eee0e2d6aff775f6f86d
Lisha from Linux-Mint-MATE-OLDHP <lishastudies.cs@gmail.com>
2026-03-15 06:23:57 +0530

    Moved testing stuff to Sandbox

--- LichenEngine/src/Lichen/EntryPoint.h
+++ LichenEngine/src/Lichen/EntryPoint.h
@@ -6,14 +6,6 @@
 
    int main() {
       Lichen::Log::Init();
-
-      int a = 100;
-      LCH_CORE_TRACE("I'M BEING TRACED D:");
-      LCH_CORE_INFO("Today is a Sunday.");
-      LCH_CORE_WARN("This is a {}", "warning");
-      LCH_FATAL("CRITICAL");
-      LCH_DEBUG("DEBUGGING {}", a);
-      LCH_ERROR("You've made an error!!!", a);
       
       Lichen::Application* app = Lichen::CreateApplication();
       app->Run();
--- Sandbox/src/SandApp.cpp
+++ Sandbox/src/SandApp.cpp
@@ -6,4 +6,14 @@ public:
    ~Sandbox() = default;
 };
 
-Lichen::Application* Lichen::CreateApplication() { return new Sandbox(); }
+Lichen::Application* Lichen::CreateApplication() { 
+   int a = 100;
+   LCH_CORE_TRACE("I'M BEING TRACED D:");
+   LCH_CORE_INFO("Today is a Sunday.");
+   LCH_CORE_WARN("This is a {}", "warning");
+   LCH_FATAL("CRITICAL");
+   LCH_DEBUG("DEBUGGING {}", a);
+   LCH_ERROR("You've made an error!!!", a);
+   
+   return new Sandbox(); 
+}
